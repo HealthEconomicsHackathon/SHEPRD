5dce4020cc1a2bd4b5035bee62c6f8f071c37cd6
Alex <alex.hill@gmail.com>
2019-11-06 12:28:33 +0000

    compile

---
+++ public/search.min.20ae6b24f4654e78be848e5dc7fd8299afe2b16a8d68e741740e53c1ed5dbddc.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/search-data.min.7993737d584f618fdcda6a190868cb4c2bcd14ddba4bb3acbb9d1afd9bdc906f.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
