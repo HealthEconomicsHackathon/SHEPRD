3cc97eb935ee287a1498f4e8f165ff9e60b62f68
Alex <alex.hill@gmail.com>
2019-11-06 12:12:57 +0000

    add public to master branch

---
+++ public/search.min.6677cf837cb0388f73f5179f1a131eca702515769d830bf3c6eb733f7751b4ab.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/search-data.min.b301769632e56c84027207484b8c7ac239384bee50df2e33e68f4c10020ba234.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
