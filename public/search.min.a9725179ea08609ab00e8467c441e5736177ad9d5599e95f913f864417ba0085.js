ca442455bfa3ed7754d0cd7146b578e0940fd999
Alex <alex.hill@gmail.com>
2019-11-07 16:09:37 +0000

    add front page blurb

---
+++ public/search.min.a9725179ea08609ab00e8467c441e5736177ad9d5599e95f913f864417ba0085.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/search-data.min.a4520053e06eff463e6620e3a7655d52301bf4b15e889ac2da5dccce83e7e5a1.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
