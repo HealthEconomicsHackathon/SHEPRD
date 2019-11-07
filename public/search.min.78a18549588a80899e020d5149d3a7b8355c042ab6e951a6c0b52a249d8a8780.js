3edf31931e8aa4c08d08a936c24ee22c772eb065
Alex <alex.hill@gmail.com>
2019-11-07 15:39:31 +0000

    add logo

---
+++ public/search.min.78a18549588a80899e020d5149d3a7b8355c042ab6e951a6c0b52a249d8a8780.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/search-data.min.ba1d7100bb779adca342ffffc38a3f9c786ffa5533231a13e2f718d05a8be40d.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
